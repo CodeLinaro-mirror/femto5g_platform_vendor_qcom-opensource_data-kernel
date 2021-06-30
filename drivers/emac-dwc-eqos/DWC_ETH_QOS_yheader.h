53d66f7e98b5bd9059d8f82e42217f44d3680776
Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>
2021-06-30 13:01:29 -0700

    Data-kernel: EMAC: Disable wake on lan at boot up
    Based on dtsi property enable\disable wol on
    boot up.
    
    Change-Id: I1f611ce82d5a8df4863fe2529b02b7d8ac7dbb0d
    Signed-off-by: Vatte Vishnuvardhan Reddy <quic_vishnuvr@quicinc.com>

--- drivers/emac-dwc-eqos/DWC_ETH_QOS_yheader.h
+++ drivers/emac-dwc-eqos/DWC_ETH_QOS_yheader.h
@@ -1749,6 +1749,8 @@ struct DWC_ETH_QOS_prv_data {
 	u32 dev_state;
 	u32 interface;
 
+	/* state of wol */
+	bool en_wol;
 	/* saving state for Wake-on-LAN */
 	int wolopts;
 	/* state of enabled wol options in PHY*/
