c1b74b34c9b67a043160145a9299e3f77038614e
Thadi Nagaraj <quic_tnagaraj@quicinc.com>
2022-03-07 17:07:43 -0800

    datacsm-kernel: ETH ECPRISS interface header file
    This commit adds interface defintions between ETH and eCPRI
    driver modules.
    
    Change-Id: Id4fada151a26cf7cde6aaae166b5b75194611372
    Signed-off-by: Thadi Nagaraj <quic_tnagaraj@quicinc.com>

---
+++ drivers/include/eth_ecpriss_iface.h
@@ -0,0 +1,205 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef ECPRI_ETH_H
+#define ECPRI_ETH_H
+
+#define ECPRISS_MAC_ADDR_LEN        6
+#define ECPRISS_MAX_PORTS           3
+#define ECPRISS_MAX_UNIQUE_PORT     3
+#define ECPRISS_MAX_LINKS           4
+
+
+typedef void (*eth_ecpriss_topology_ready_cb)(void);
+
+/**
+ * enum dev mode
+ */
+typedef enum {
+	ETH_ECPRISS_DEV_MODE_DU = 0,
+	ETH_ECPRISS_DEV_MODE_RU,
+	ETH_ECPRISS_DEV_MODE_MAX
+} eth_ecpriss_dev_mode_e;
+
+/**
+ * enum eth_ecpriss_event_e - event type
+ */
+typedef enum {
+	ETH_ECPRISS_EVENT_UP = 0,
+	ETH_ECPRISS_EVENT_DOWN,
+	ETH_ECPRISS_EVENT_DESTROY,
+	ETH_ECPRISS_EVENT_MAX
+} eth_ecpriss_event_e;
+
+/**
+ * enum eth_ecpriss_status_e - status type
+ */
+typedef enum {
+	ETH_ECPRISS_STATUS_SUCCESS = 0,
+	ETH_ECPRISS_STATUS_FAILURE,
+	ETH_ECPRISS_STATUS_MAX
+} eth_ecpriss_status_e;
+
+/**
+ * enum eth_ecpriss_port_type - port type
+ */
+typedef enum {
+	ETH_ECPRISS_PORT_TYPE_FH = 0,
+	ETH_ECPRISS_PORT_TYPE_C2C,
+	ETH_ECPRISS_PORT_TYPE_L2,
+	ETH_ECPRISS_PORT_TYPE_MAX,
+} eth_ecpriss_port_type_e;
+
+/**
+ * enum eth_ecpriss_link_type - link type
+ */
+typedef enum {
+	ETH_ECPRISS_LINK_RATE_1000 = 0,
+	ETH_ECPRISS_LINK_RATE_100,
+	ETH_ECPRISS_LINK_RATE_25,
+} eth_ecpriss_link_rate_e;
+
+/**
+ * ecpriss_eth_link_params
+ * This struct is used for the link parameter values from the
+ * topology.
+ * link_index - link index associated with the specific link
+ * eth_mac_address - mac address of the link
+ * link_rate - speed of the link associated with the link
+ */
+
+typedef struct eth_ecpriss_link_params {
+	uint8_t                          link_index;
+	uint16_t                         link_mtu;
+	uint8_t                          eth_mac_addr[ECPRISS_MAC_ADDR_LEN];
+	uint16_t                         link_state;
+	eth_ecpriss_link_rate_e          link_rate;
+} eth_ecpriss_link_params_s;
+
+/**
+ * ecpriss_eth_port_params
+ * This struct is used for the port parameter values from the
+ * topology.
+ * num_links - Total number of links associated with the port
+ * link_params - Values associated with each link
+ */
+typedef struct eth_ecpriss_port_params {
+	uint8_t                        port_index;
+	uint8_t                        num_links;
+	eth_ecpriss_link_params_s      link_params[ECPRISS_MAX_LINKS];
+} eth_ecpriss_port_params_s;
+
+
+/**
+ * ecpriss_eth_topology_params
+ * This struct is used for the topology values from the
+ *  topology.
+ * port_type - Type of port - FH/C2C/L2
+ * num_ports- Number of active ports associated
+ * port_params -number of links per port and associated params,
+ * index corresponds to the port index
+ */
+typedef struct eth_ecpriss_topology_params {
+	eth_ecpriss_port_type_e        port_type;
+	uint8_t                        num_ports; //3 FH ports
+	eth_ecpriss_port_params_s      port_params[ECPRISS_MAX_PORTS];
+} eth_ecpriss_topology_params_s;
+
+/**
+ * ecpriss_eth_topology_params
+ * This struct is used for the topology values from the
+ * topology.
+ * num_unique_port_types- unique port such as
+ * L2/FH/C2C e.g 2 if there is 1 FH and 1 C2C
+ * topology_params- Consists of different topology params
+ */
+typedef struct eth_ecpriss_topology_root {
+	bool                            eth_topology_init_done;
+	uint8_t                         num_unique_port_types;
+	eth_ecpriss_topology_params_s
+			topology_params[ECPRISS_MAX_UNIQUE_PORT];
+} eth_ecpriss_topology_root_s;
+
+
+/**
+ * eth_ecpriss_register_events_params
+ * Port and link information for incoming events
+ * port_num - registers for IF up/down event notification
+ * link_id - link which has received the up/down event
+ * port_type - Type of port
+ */
+typedef struct eth_ecpriss_link_event_params {
+	uint8_t                        port_index;
+	uint8_t                        link_index;
+	eth_ecpriss_port_type_e        port_type;
+} eth_ecpriss_link_event_params_s;
+
+
+
+typedef void (*eth_ecpriss_interface_events_cb)
+		(eth_ecpriss_event_e event_type,
+		eth_ecpriss_link_event_params_s *link_event_params);
+
+
+/**
+ * eth_ecpriss_register_topology_cb() - ECPRISS driver
+ * registration with ETH driver for topology
+ * @eth_ecpriss_register_topology_params - Register for the
+ * topology CB and provide the params
+ * Registration for topology callback is done via this function.
+ * When the topology is available the ethernet invokes this
+ * callback function and provides the required parameters.
+ *
+ *
+ * Returns:	0 on success, negative on failure
+ */
+eth_ecpriss_status_e eth_ecpriss_register_ready_cb
+		(eth_ecpriss_topology_ready_cb ready_cb, bool *is_ready);
+
+
+/**
+ * eth_ecpriss_get_stats() - ETH driver will provide the
+ * relevant stats to the ECPRISS driver.
+ *
+ * Device_mode : DU or RU operating mode
+ * num_port_type:Number of unique port types for the device
+ * (FH, C2C, L2)
+ *
+ * Returns:	0 on success, negative on failure
+ */
+eth_ecpriss_status_e eth_ecpriss_get_topology
+		(eth_ecpriss_dev_mode_e *device_mode,
+		eth_ecpriss_topology_root_s   *topology_params);
+
+
+/**
+ * eth_ecpriss_register_events_cb() - ECPRISS driver
+ * registration with ETH driver for events
+ * @eth_ecpriss_register_topology_params - Register for the
+ * required events CB and provide the params
+ * Registration for events callback is done via this function.
+ * For up/down events the ecpriss driver will obtain the callback
+ * from the ethernet driver
+ *
+ * Returns:	0 on success, negative on failure
+ */
+eth_ecpriss_status_e eth_ecpriss_register_events_cb
+			(eth_ecpriss_interface_events_cb events_cb);
+
+/**
+ * struct eth_ecpriss_ops - Structure to hold ETH - eCPRI
+ * interface functions
+ */
+struct eth_ecpriss_ops {
+	eth_ecpriss_status_e (*eth_ecpriss_register_ready_cb)
+		(eth_ecpriss_topology_ready_cb ready_cb, bool *is_ready);
+	eth_ecpriss_status_e (*eth_ecpriss_register_events_cb)
+		(eth_ecpriss_interface_events_cb events_cb);
+	eth_ecpriss_status_e (*eth_ecpriss_get_topology)
+		(eth_ecpriss_dev_mode_e *device_mode,
+		 eth_ecpriss_topology_root_s *topology_params);
+};
+
+#endif // ECPRI_ETH_H
