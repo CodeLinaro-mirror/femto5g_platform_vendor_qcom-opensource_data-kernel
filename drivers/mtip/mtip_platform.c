bd6ed7842a9ee0bc5cdc129403858a7ccabdba71
Thadi Nagaraj <quic_tnagaraj@quicinc.com>
2022-07-14 16:27:03 -0700

    PTP timestamping enablement
    Change-Id: Iea7cf9f0742e7f5761dfdce7330dc381eed498eb

--- drivers/mtip/mtip_platform.c
+++ drivers/mtip/mtip_platform.c
@@ -1025,8 +1025,9 @@ static int mtip_platform_setup(void)
      // register for MAC wrapper IRQ
      mtip_mac_wrapper_register_irq(&platform_driver_priv->devices.port_devices[i].port_pdev->dev, 
                                     platform_driver_priv->devices.port_devices[i].wrapper_irq, 
-                                    NULL, 
+                                    DRV_NAME, 
                                     (void *)&platform_driver_priv->devices.port_devices[i]);
+
      if (mtip_rumi_platform == 0) 
      {
          // set the mac wrapper pcs mode control
@@ -1154,6 +1155,9 @@ static int mtip_platform_setup(void)
                   // enable pcs loopback on the link
                   mtip_pcs_enable_loopback(i);
               }
+
+              // set the MAC interrupt mask
+              mtip_mac_set_interrupt_mask(i);
           }
 
           // add the mtip_napi_rx
