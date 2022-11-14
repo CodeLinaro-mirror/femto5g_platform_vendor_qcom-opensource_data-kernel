cf01e4563022b4a5f2da2683a83b8638c64dcfd9
quic_dkotla <quic_dkotla@quicinc.com>
2022-11-14 07:14:11 -0800

    Revert "datacsm-kernel: qcom-aw-phy: Fixed CDR lock callback handling for Debug FS."
    This reverts commit be85a0a9014bb7e7e88f997e8d0ac1da2616bbe0.
    
    Change-Id: Ibc4ba564ad6b26c009f3f522f575c731ed306934

--- drivers/qcom_aw_phy/qcom_aw_phy_test_sysfs.c
+++ drivers/qcom_aw_phy/qcom_aw_phy_test_sysfs.c
@@ -175,11 +175,6 @@ void qcom_aw_phy_an_complete_cb(enum mtip_port_type_enum port_type,
                       lane_num);
 }
 
-void qcom_aw_phy_cdr_lock_cb(enum mtip_port_type_enum port_type,
-                                enum eth_phy_iface_phy_lane_num_enum lane_num) {
-  QCOM_AW_PHY_LOG_ERR("CDR lock success port %d, lane %d", port_type,
-                      lane_num);
-}
 ssize_t qcom_aw_phy_get_prbs_result(struct file *file, char __user *buf,
                                     size_t count, loff_t *ppos){
   char dbg_buf[1500] = {0};
@@ -261,7 +256,6 @@ ssize_t qcom_aw_phy_set_attr(struct file *file, const char __user *buf,
       ready_info.notify_ready = qcom_aw_phy_ready_cb;
       ready_info.userdata_ready = NULL;
       ready_info.notify_an_complete = qcom_aw_phy_an_complete_cb;
-      ready_info.cdr_lock_success = qcom_aw_phy_cdr_lock_cb;
       qcom_aw_phy_driver_iface_ops.eth_phy_iface_eth_register(&ready_info,
                                                               &is_phy_ready);
       QCOM_AW_PHY_LOG_ERR("is_phy_ready %d", is_phy_ready);
