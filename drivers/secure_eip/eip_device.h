12c681bd97f74a1f972a0b5aa7262901a5659c5e
Jinesh K. Jayakumar <quic_jineshk@quicinc.com>
2023-01-07 15:09:47 -0500

    drivers: secure_eip: Use the new mtip_security API
    Enable support in EIP driver to use the new mtip_security API for
    security device registration.
    
    Change-Id: Ia4f9ad2b31a63bfd3d4d89e3606bd9a05904b6b4
    Signed-off-by: Jinesh K. Jayakumar <quic_jineshk@quicinc.com>

---
+++ drivers/secure_eip/eip_device.h
@@ -0,0 +1,43 @@
+/* SPDX-License-Identifier: GPL-2.0-only
+ * Copyright (c) 2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _EIP_DEVICE_H_
+#define _EIP_DEVICE_H_
+
+#include <linux/netdevice.h>
+
+#include <mtip_security.h>
+
+struct eip_port;
+
+struct eip_datapath {
+	int devid;
+	struct eip_port *port;
+};
+
+struct eip_port {
+	int id;
+
+	struct eip_datapath rx;
+	struct eip_datapath tx;
+
+	struct mtip_security_device msec_dev;
+};
+
+struct eip_channel {
+	struct eip_datapath *dp;
+	int ch;
+};
+
+#define eip_chid(eip_ch) ((eip_ch)->ch)
+#define eip_devid(eip_ch) ((eip_ch)->dp->devid)
+
+/* Represents the pair of EIP channels associated with a net device */
+struct eip_link {
+	struct net_device *ndev;
+	struct eip_channel rx;
+	struct eip_channel tx;
+};
+
+#endif /* _EIP_DEVICE_H_ */
