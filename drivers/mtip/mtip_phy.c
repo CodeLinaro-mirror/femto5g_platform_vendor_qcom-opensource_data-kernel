96dda5a569decf9d30a2ddeb6844951a9b157265
Nagarjuna Chaganti <quic_nchagant@quicinc.com>
2023-08-02 22:18:38 -0700

    datacsm-kernel: mtip: Added a workaround check for NULL pointer access in Lane down handling.
    Change-Id: I4e012155736140107d7943ec870c2cdb1f4b64ee

--- drivers/mtip/mtip_phy.c
+++ drivers/mtip/mtip_phy.c
@@ -1,6 +1,6 @@
 //SPDX-License-Identifier: GPL-2.0-only
 /*
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2023 Qualcomm Innovation Center, Inc. All rights reserved.
  */ 
 
 #include <linux/init.h>
@@ -574,13 +574,16 @@ static void mtip_phy_handle_lane_up(u32 lane_index, u8 sfp_port_type, enum eth_p
 
 static void mtip_phy_handle_lane_down(u32 lane_index)
 {
-   enum mtip_lane_state_enum current_state;
    int i;
    u32 link_index;
    enum mtip_link_state_enum link_state;
    u32 port_type;
+   enum mtip_lane_state_enum current_state = MTIP_LANE_STATE_INIT;
 
-   current_state = platform_driver_priv->mtip_lanes[lane_index]->lane_state;
+   if(platform_driver_priv->mtip_lanes[lane_index])
+   {
+      current_state = platform_driver_priv->mtip_lanes[lane_index]->lane_state;
+   }
 
    switch (current_state)
    {
@@ -588,7 +591,10 @@ static void mtip_phy_handle_lane_down(u32 lane_index)
    case MTIP_LANE_STATE_INIT:
       {
          CSMLOGINFO("Handling transition from lane state %d to DISCONNECTED for lane: %d", current_state, lane_index);
-         platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED;
+	 if(platform_driver_priv->mtip_lanes[lane_index])
+         {
+           platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED; 
+         }
       }
       break;
 
@@ -597,7 +603,10 @@ static void mtip_phy_handle_lane_down(u32 lane_index)
          CSMLOGINFO("Handling transition from CONNECTED to DISCONNECTED for lane: %d", lane_index);
 
          // set the lane state as DISCONNECTED
-         platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED;
+         if(platform_driver_priv->mtip_lanes[lane_index])
+         {
+           platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED;
+         }
 
          // determine the port using lane
          mtip_lookup_port_type_by_lane_index(lane_index, &port_type);
@@ -635,8 +644,11 @@ static void mtip_phy_handle_lane_down(u32 lane_index)
 
    default:
       {
-         CSMLOGINFO("Handling transition from unknown to CONNECTED for lane: %d", lane_index);
-         platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED;
+       CSMLOGINFO("Handling transition from unknown to CONNECTED for lane: %d", lane_index);
+       if(platform_driver_priv->mtip_lanes[lane_index])  
+       { 
+         platform_driver_priv->mtip_lanes[lane_index]->lane_state = MTIP_LANE_STATE_DISCONNECTED; 
+       }
       }
       break;
    }
