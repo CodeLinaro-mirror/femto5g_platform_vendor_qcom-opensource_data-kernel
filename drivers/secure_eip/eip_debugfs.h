1fc25f86a58f6436abfe96bffe24fd4e79c5499b
Jagannath Sahoo <quic_jagasaho@quicinc.com>
2023-11-06 12:57:32 +0530

    driver: secure_eip: dumping transform record
    Change added to print transform records using debugfs for both
    MACSec and IPsec modes.
    
    Signed-off-by: Jagannath Sahoo <quic_jagasaho@quicinc.com>
    Change-Id: I40f908351b8252ef6a9e9f50a1135c7ed44211d1

--- drivers/secure_eip/eip_debugfs.h
+++ drivers/secure_eip/eip_debugfs.h
@@ -13,6 +13,8 @@ int eip_debugfs_init(void);
 void eip_debugfs_deinit(void);
 int eip_debugfs_add_port(struct eip_port *port);
 void eip_debugfs_remove_port(struct eip_port *port);
+int eip_debugfs_add_link(struct eip_link *link);
+void eip_debugfs_remove_link(struct eip_link *link);
 #else
 static inline int eip_debugfs_init(void)
 {
@@ -26,6 +28,13 @@ static inline int eip_debugfs_add_port(struct eip_port *port)
 	return 0;
 }
 static inline void eip_debugfs_remove_port(struct eip_port *port)
+{
+}
+static int eip_debugfs_add_link(struct eip_link *link)
+{
+	return 0;
+}
+static int eip_debugfs_remove_link(struct eip_link *link)
 {
 }
 #endif
--- drivers/secure_eip/eip_device.h
+++ drivers/secure_eip/eip_device.h
@@ -33,6 +33,7 @@ struct eip_port;
 struct eip_datapath {
 	int devid;
 	struct eip_port *port;
+	struct dentry *dbgfs_dp_dent;
 };
 
 struct eip_port {
@@ -50,7 +51,10 @@ struct eip_port {
 
 struct eip_channel {
 	struct eip_datapath *dp;
+	struct net_device *ndev;
 	int ch;
+	struct dentry *dbgfs_ch_dent;
+	bool inbound;
 };
 
 #define eip_chid(eip_ch) ((eip_ch)->ch)
@@ -63,6 +67,8 @@ struct eip_link {
 	struct eip_channel tx;
 
 	void *ipsec_priv;
+
+	struct dentry *dbgfs_dent;
 };
 
 #endif /* _EIP_DEVICE_H_ */
