45f91b8e4f13a34ab3f10d21badb433ccd242681
Priya Tripathi <quic_ppriyatr@quicinc.com>
2024-02-13 21:57:18 +0530

    datacsm-kernel: mtip: Fallback to default FEC mode for 40G in case of mismatched FEC mode on remote end
    Following fixed are done:
    1. Advertise BASER FEC to remote end for 40G BASER FEC mode.
    2. Fallback to non-fec mode if remote end has mismatched fec mode in case of autoneg mode
    
    Change-Id: I28d9d3d9a95544bbbb7cbef108ebc2128bf2d9f7

--- drivers/qcom_aw_phy/qcom_aw_phy_mtip_if.c
+++ drivers/qcom_aw_phy/qcom_aw_phy_mtip_if.c
@@ -473,9 +473,14 @@ int qcom_aw_phy_configure_speed_mode(
           break;
 
         case MTIP_PORT_CONFIG_1x40GBASE_R4:
+          phy_inst_info->an_params.adv_ability[PHY_SPEED_SPEC_40G_BASE_KR4] = 1;
+          phy_inst_info->an_params.adv_ability[PHY_SPEED_SPEC_40G_BASE_CR4] = 1;
+          break;
+
         case MTIP_PORT_CONFIG_1x40GBASE_R4_FEC:
           phy_inst_info->an_params.adv_ability[PHY_SPEED_SPEC_40G_BASE_KR4] = 1;
           phy_inst_info->an_params.adv_ability[PHY_SPEED_SPEC_40G_BASE_CR4] = 1;
+          phy_inst_info->an_params.fec_ability[PHY_10G_BASE_R_FEC] = 1;
           break;
 
         case MTIP_PORT_CONFIG_1x25GBASE_R:
@@ -629,10 +634,17 @@ enum mtip_port_config_enum qcom_aw_phy_an_result_to_port_config(
 
     case PHY_SPEED_SPEC_40G_BASE_KR4:
     case PHY_SPEED_SPEC_40G_BASE_CR4:
-      if(phy_inst_info->an_params.mac_port_config_mask & (1<<MTIP_PORT_CONFIG_1x40GBASE_R4_FEC))
-        return MTIP_PORT_CONFIG_1x40GBASE_R4_FEC;
-      else
-        return MTIP_PORT_CONFIG_1x40GBASE_R4;
+      if((phy_inst_info->an_params.mac_port_config_mask & (1<<MTIP_PORT_CONFIG_1x40GBASE_R4)) ||
+         (phy_inst_info->an_params.mac_port_config_mask & (1<<MTIP_PORT_CONFIG_1x40GBASE_R4_FEC))){
+
+        if(phy_inst_info->an_params.fec_ability[PHY_10G_BASE_R_FEC] == 1 &&
+           phy_inst_info->an_params.lp_fec_ability[PHY_LANE_0][PHY_10G_BASE_R_FEC] == 1){
+          phy_inst_info->an_params.an_fec_result[PHY_LANE_0] = PHY_10G_BASE_R_FEC;
+          return MTIP_PORT_CONFIG_1x40GBASE_R4_FEC;
+	}
+        else
+          return MTIP_PORT_CONFIG_1x40GBASE_R4;
+        }
 
     case PHY_SPEED_SPEC_50G_BASE_K_CR:
       if(phy_inst_info->an_params.mac_port_config_mask & (1<<MTIP_PORT_CONFIG_2x50GBASE_R_RSFEC))
