7a2563a8643bc9b480ab6967766df1afe590f727
Rohit Tomar <quic_rtomar@quicinc.com>
2024-03-28 02:27:14 -0700

    ecpri_mhi:dpdk-ethtool: Add retries to wait for buffer replenishment for                         write control channels.
    Change-Id: Ic9540788d60fb36d380f1973e1b4d7d800e8f35b

--- drivers/ecpri/ecpri_core/src/ecpriss_mhi.c
+++ drivers/ecpri/ecpri_core/src/ecpriss_mhi.c
@@ -1051,6 +1051,8 @@ int ecpriss_mhi_send_rsp(void *buf, int len,
 
 	struct mhi_req req;
 
+	int retry_count = ECPRISS_MAX_MHI_SEND_RETRY_CNT;
+
 	int err = -1;
 
 	if (!ch_info || atomic_read(&ch_info->ch_state) != ECPRISS_MHI_CH_STATE_OPEN) {
@@ -1071,9 +1073,21 @@ int ecpriss_mhi_send_rsp(void *buf, int len,
 	req.client = ch_info->handle;
 	req.vf_id = ch_info->vf_id;
 
-	bytes_written = mhi_dev_write_channel(&req);
+	/* These retries are needed if we don't have any
+	 * buffers available from host to write*/
+
+	do {
+
+		bytes_written = mhi_dev_write_channel(&req);
+
+		retry_count--;
+
+		msleep(ECPRISS_MAX_MHI_SEND_RETRY_DELAY);
+
+	} while (bytes_written == 0 && retry_count);
 
 	ECPRILOGDBG("MHI SND RESP: Bytes to send %d  and bytes Written %d\n", req.len, bytes_written);
+
 
 	if(bytes_written != len) {
 
